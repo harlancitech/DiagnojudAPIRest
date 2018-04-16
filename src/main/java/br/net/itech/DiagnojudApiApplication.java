2ea095ac2ee03a21ac1acec42a6720b6b4a6f873
Harlan Cleiton <harlancleiton@gmail.com>
2018-04-16 07:42:43 -0300

    First Commit

---
+++ src/main/java/br/net/itech/DiagnojudApiApplication.java
@@ -0,0 +1,12 @@
+package br.net.itech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnojudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnojudApiApplication.class, args);
+	}
+}
